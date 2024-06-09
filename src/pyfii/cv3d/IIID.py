352ac99e8fb44eed522106781c54c1b1c692f74f
miaooo0000OOOO <1683618861@qq.com>
2024-06-09 23:46:21 +0800

    重构cv3d_IID_transfer

--- src/pyfii/cv3d/IIID.py
+++ src/pyfii/cv3d/IIID.py
@@ -1,141 +1,441 @@
+from typing import Any, List, Tuple
 import cv2
+from cv2.typing import MatLike, Scalar
 import numpy as np
 from .transfer import *
-def eye_vector(a,b):
-    return (np.cos(np.radians(abs(a)))*np.cos(np.radians(abs(b))),np.sin(np.radians(abs(a)))*np.cos(np.radians(abs(b))),np.sin(np.radians(abs(b))))
-def eye_axis(a,b,d0):
-    return (-np.cos(np.radians(abs(a)))*np.cos(np.radians(abs(b)))*d0,-np.sin(np.radians(abs(a)))*np.cos(np.radians(abs(b)))*d0,-np.sin(np.radians(abs(b)))*d0)
-def abs_3d_vector(v):
-    return (v[0]**2+v[1]**2+v[2]**2)**0.5
-def dot_3d_v1_v2(v1,v2):
-    return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2]
-def iiid2iid(aixs,x,y,a,b,center=(0,0,0),d=(1,0)):#三维坐标点投影在二维平面的位置(三维坐标点,二维平面原点xy,观察者面朝角度ab,观察者观察的中心位置)
-    a=np.radians(a)
-    b=np.radians(b)
-    aixs=rotate3d((aixs[0]-center[0],aixs[1]-center[1],aixs[2]-center[2]),-a,-b)
-    if d[1]==0:
-        return x-aixs[1]*d[0],y-aixs[2]*d[0],aixs[0]#第一二个输出为显示的xy坐标,第三个输出为点与观察者的相对距离(用于确定渲染的先后顺序,远先近后)
-    elif aixs[0]+d[0]==0:
-        return 0,0,0
+
+# 类型别名 为了兼容3.12以下Python版本没有使用type Alias = ...语法
+
+Vec3 = np.ndarray | Tuple[float, float, float] | List[float, float, float]
+Degree = float  # 角度制浮点数
+Radian = float  # 弧度制浮点数
+
+
+def eye_vector(a: Degree, b: Degree) -> Tuple[float, float, float]:
+    return (
+        np.cos(np.radians(abs(a))) * np.cos(np.radians(abs(b))),
+        np.sin(np.radians(abs(a))) * np.cos(np.radians(abs(b))),
+        np.sin(np.radians(abs(b))),
+    )
+
+
+def eye_axis(a: Degree, b: Degree, d0: float) -> Tuple[float, float, float]:
+    return (
+        -np.cos(np.radians(abs(a))) * np.cos(np.radians(abs(b))) * d0,
+        -np.sin(np.radians(abs(a))) * np.cos(np.radians(abs(b))) * d0,
+        -np.sin(np.radians(abs(b))) * d0,
+    )
+
+
+def abs_3d_vector(v: Vec3) -> float:
+    """取得三维向量的模长
+
+    Args:
+        v (Vec3): 三维向量
+
+    Returns:
+        float: 三维向量的模长
+    """
+    assert len(v) == 3
+    return np.linalg.norm(v)
+
+
+def dot_3d_v1_v2(v1: Vec3, v2: Vec3) -> float:
+    """两个三维向量的点乘
+
+    Args:
+        v1 (Vec3): 三维向量
+        v2 (Vec3): 三维向量
+
+    Returns:
+        float: 点乘（内积）
+    """
+    assert len(v1) == 3 and len(v2) == 3
+    return np.dot(v1, v2)
+
+
+def iiid2iid(
+    point: Vec3,
+    x: float,
+    y: float,
+    a: Degree,
+    b: Degree,
+    center: Tuple[float, float, float] = (0, 0, 0),
+    d: Tuple[float, float] = (1, 0),
+) -> Tuple[float, float, float]:
+    """三维坐标点投影在二维平面的位置(三维坐标点,二维平面原点xy,观察者面朝角度ab,观察者观察的中心位置)
+
+    Args:
+        point (Vec3): 三维坐标点
+        x (float): 二维平面原点x
+        y (float): 二维平面原点y
+        a (Degree): 观察者面朝角度a
+        b (Degree): 观察者面朝角度b
+        center (Tuple[float, float, float], optional): 观察者观察的中心位置. Defaults to (0, 0, 0).
+        d (Tuple[float, float], optional): _description_. Defaults to (1, 0).
+
+    Returns:
+        Tuple[ float, float, float ]: 三维坐标点投影在二维平面的位置
+    """
+    a = np.radians(a)
+    b = np.radians(b)
+    point = rotate3d(
+        (point[0] - center[0], point[1] - center[1], point[2] - center[2]), -a, -b
+    )
+    if d[1] == 0:
+        return (
+            x - point[1] * d[0],
+            y - point[2] * d[0],
+            point[0],
+        )  # 第一二个输出为显示的xy坐标,第三个输出为点与观察者的相对距离(用于确定渲染的先后顺序,远先近后)
+    elif point[0] + d[0] == 0:
+        return 0, 0, 0
     else:
-        return x-aixs[1]*d[1]/(aixs[0]+d[0]),y-aixs[2]*d[1]/(aixs[0]+d[0]),aixs[0]+d[0]#第一二个输出为显示的xy坐标,第三个输出为点与观察者的相对距离(用于确定渲染的先后顺序,远先近后)
-def sphere(img,aixs,x,y,a,b,color,r=1,center=(0,0,0),n=-1,d=(1,0)):#在img上显示球体（点）(img,球心坐标,二维平面原点xy,观察者面朝角度ab,颜色,半径,观察者观察的中心位置)
-    x1,y1,z=iiid2iid(aixs,x,y,a,b,center,d)
-    if d[1]==0:
-        cv2.circle(img,(int(x1),int(y1)),r,color,n)
+        return (
+            x - point[1] * d[1] / (point[0] + d[0]),
+            y - point[2] * d[1] / (point[0] + d[0]),
+            point[0] + d[0],
+        )  # 第一二个输出为显示的xy坐标,第三个输出为点与观察者的相对距离(用于确定渲染的先后顺序,远先近后)
+
+
+def sphere(
+    img: MatLike,
+    point: Vec3,
+    x: float,
+    y: float,
+    a: Degree,
+    b: Degree,
+    color: Scalar,
+    r: int = 1,
+    center: Tuple[float, float, float] = (0, 0, 0),
+    thickness: int = -1,
+    d: Tuple[float, float] = (1, 0),
+):
+    """在图像上显示球体（点）(img,球心坐标,二维平面原点xy,观察者面朝角度ab,颜色,半径,观察者观察的中心位置)
+
+    Args:
+        img (MatLike): 图像
+        point (Vec3): 球心坐标
+        x (float): 二维平面原点x
+        y (float): 二维平面原点y
+        a (Degree): 观察者面朝角度a
+        b (Degree): 观察者面朝角度b
+        color (Scalar): 颜色
+        r (int, optional): 半径. Defaults to 1.
+        center (Tuple[float, float, float], optional): 观察者观察的中心位置. Defaults to (0, 0, 0).
+        thickness (int, optional): 绘制的线宽，默认实心. Defaults to -1.
+        d (Tuple[float, float], optional): _description_. Defaults to (1, 0).
+    """
+    x1, y1, z = iiid2iid(point, x, y, a, b, center, d)
+    if d[1] == 0:
+        cv2.circle(img, (int(x1), int(y1)), r, color, thickness)
     else:
-        if z>0:
-            cv2.circle(img,(int(x1),int(y1)),int(r*d[1]/z+1),color,n)
-def line(img,aixs1,aixs2,x,y,a,b,color,r=1,center=(0,0,0),t=8,d=(1,0)):#在img上显示线段(img,端点1坐标,端点2坐标,二维平面原点xy,观察者面朝角度ab,颜色,粗细,观察者观察的中心位置,直线类型)
-    x1,y1,z1=iiid2iid(aixs1,x,y,a,b,center,d)
-    x2,y2,z2=iiid2iid(aixs2,x,y,a,b,center,d)
-    if d[1]==0:
-        cv2.line(img,(int(x1),int(y1)),(int(x2),int(y2)),color,r,t)
+        if z > 0:
+            cv2.circle(img, (int(x1), int(y1)), int(r * d[1] / z + 1), color, thickness)
+
+
+def line(
+    img: MatLike,
+    point1: Vec3,
+    point2: Vec3,
+    x: float,
+    y: float,
+    a: Degree,
+    b: Degree,
+    color: Scalar,
+    thickness: int = 1,
+    center: Tuple[float, float, float] = (0, 0, 0),
+    line_type: int = cv2.LINE_8,
+    d: Tuple[float, float] = (1, 0),
+):
+    """在图像上显示线段(img,端点1坐标,端点2坐标,二维平面原点xy,观察者面朝角度ab,颜色,粗细,观察者观察的中心位置,直线类型)
+
+    Args:
+        img (MatLike): 图像
+        point1 (Vec3): 端点1坐标
+        point2 (Vec3): 端点2坐标
+        x (float): 二维平面原点x
+        y (float): 二维平面原点y
+        a (Degree): 观察者面朝角度a
+        b (Degree): 观察者面朝角度b
+        color (Scalar): 颜色
+        thickness (int, optional): 绘制的线宽，默认实心. Defaults to 1.
+        center (Tuple[float, float, float], optional): 观察者观察的中心位置. Defaults to (0, 0, 0).
+        line_type (int, optional): 线的类型. Defaults to cv2.LINE_8.
+        d (Tuple[float, float], optional): _description_. Defaults to (1, 0).
+    """
+    x1, y1, z1 = iiid2iid(point1, x, y, a, b, center, d)
+    x2, y2, z2 = iiid2iid(point2, x, y, a, b, center, d)
+    if d[1] == 0:
+        cv2.line(
+            img, (int(x1), int(y1)), (int(x2), int(y2)), color, thickness, line_type
+        )
     else:
-        if z1>0 and z2>0:
-            cv2.line(img,(int(x1),int(y1)),(int(x2),int(y2)),color,r,t)
-def ring(img,aixs,x,y,a,b,color,r,center=(0,0,0),n=1,d=(1,0),normal_vector=(0,0,1)):
-    x1,y1,z=iiid2iid(aixs,x,y,a,b,center,d)
-    if d[1]==0:
-        eye_vec=eye_vector(a,b)
-        ratio_ellipse_a_b=abs(dot_3d_v1_v2(eye_vec,normal_vector)/abs_3d_vector(normal_vector))
-        if int(r*ratio_ellipse_a_b+0.5)<1:
-            cv2.ellipse(img,(int(x1),int(y1)),(int(r+0.5),1),0,0,360,color,n)
+        if z1 > 0 and z2 > 0:
+            cv2.line(
+                img, (int(x1), int(y1)), (int(x2), int(y2)), color, thickness, line_type
+            )
+
+
+def ring(
+    img: MatLike,
+    point: Vec3,
+    x: float,
+    y: float,
+    a: Degree,
+    b: Degree,
+    color: Scalar,
+    r: int,
+    center: Tuple[float, float, float] = (0, 0, 0),
+    thickness: int = 1,
+    d: Tuple[float, float] = (1, 0),
+    normal_vector: Vec3 = (0, 0, 1),
+):
+    """在图像上显示圆环
+
+    Args:
+        img (MatLike): 图像
+        point (Vec3): 圆心坐标
+        x (float): 二维平面原点x
+        y (float): 二维平面原点y
+        a (Degree): 观察者面朝角度a
+        b (Degree): 观察者面朝角度b
+        color (Scalar): 颜色
+        r (int): 半径
+        center (Tuple[float, float, float], optional): 观察者观察的中心位置. Defaults to (0, 0, 0).
+        thickness (int, optional): 绘制的线宽，默认实心. Defaults to 1.
+        d (Tuple[float, float], optional): _description_. Defaults to (1, 0).
+        normal_vector (Vec3, optional): 圆环平面的法向量. Defaults to (0, 0, 1).
+    """
+    x1, y1, z = iiid2iid(point, x, y, a, b, center, d)
+    if d[1] == 0:
+        eye_vec = eye_vector(a, b)
+        ratio_ellipse_a_b = abs(
+            dot_3d_v1_v2(eye_vec, normal_vector) / abs_3d_vector(normal_vector)
+        )
+        if int(r * ratio_ellipse_a_b + 0.5) < 1:
+            cv2.ellipse(
+                img, (int(x1), int(y1)), (int(r + 0.5), 1), 0, 0, 360, color, thickness
+            )
         else:
-            cv2.ellipse(img,(int(x1),int(y1)),(int(r+0.5),int(r*ratio_ellipse_a_b+0.5)),0,0,360,color,n)
+            cv2.ellipse(
+                img,
+                (int(x1), int(y1)),
+                (int(r + 0.5), int(r * ratio_ellipse_a_b + 0.5)),
+                0,
+                0,
+                360,
+                color,
+                thickness,
+            )
     else:
-        a=np.radians(a)
-        b=np.radians(b)
-        aixs=rotate3d((aixs[0]-center[0],aixs[1]-center[1],aixs[2]-center[2]),-a,-b)
-        where_eye_is=eye_axis(a,b,d[0])
-        eye_vec=(aixs[0]-where_eye_is[0],aixs[1]-where_eye_is[1],aixs[2]-where_eye_is[2])
-        ratio_ellipse_a_b=abs(dot_3d_v1_v2(eye_vec,normal_vector)/abs_3d_vector(normal_vector)/abs_3d_vector(eye_vec))
-        if z>0:
-            if int(r*d[1]/z*ratio_ellipse_a_b+0.5)<1:
-                cv2.ellipse(img,(int(x1),int(y1)),(int(r*d[1]/z+0.5),1),0,0,360,color,n)
+        a = np.radians(a)
+        b = np.radians(b)
+        point = rotate3d(
+            (point[0] - center[0], point[1] - center[1], point[2] - center[2]), -a, -b
+        )
+        where_eye_is = eye_axis(a, b, d[0])
+        eye_vec = (
+            point[0] - where_eye_is[0],
+            point[1] - where_eye_is[1],
+            point[2] - where_eye_is[2],
+        )
+        ratio_ellipse_a_b = abs(
+            dot_3d_v1_v2(eye_vec, normal_vector)
+            / abs_3d_vector(normal_vector)
+            / abs_3d_vector(eye_vec)
+        )
+        if z > 0:
+            if int(r * d[1] / z * ratio_ellipse_a_b + 0.5) < 1:
+                cv2.ellipse(
+                    img,
+                    (int(x1), int(y1)),
+                    (int(r * d[1] / z + 0.5), 1),
+                    0,
+                    0,
+                    360,
+                    color,
+                    thickness,
+                )
             else:
-                cv2.ellipse(img,(int(x1),int(y1)),(int(r*d[1]/z+0.5),int(r*d[1]/z*ratio_ellipse_a_b+0.5)),0,0,360,color,n)
-def distance(aixs,x,y,A,B,center=(0,0,0),d=(1,0)):#计算距离
-    if aixs[-1]=='text':
+                cv2.ellipse(
+                    img,
+                    (int(x1), int(y1)),
+                    (
+                        int(r * d[1] / z + 0.5),
+                        int(r * d[1] / z * ratio_ellipse_a_b + 0.5),
+                    ),
+                    0,
+                    0,
+                    360,
+                    color,
+                    thickness,
+                )
+
+
+def distance(obj3d: List[Any], x, y, A, B, center=(0, 0, 0), d=(1, 0)):  # 计算距离
+    if obj3d[-1] == "text":
         return -10000
-    elif aixs[-1]=='line':
-        return iiid2iid(((aixs[0][0]+aixs[1][0])/2,(aixs[0][1]+aixs[1][1])/2,(aixs[0][2]+aixs[1][2])/2),x,y,A,B,center,d)[2]
-    elif aixs[-1]=='sphere':
-        return iiid2iid(aixs[0],x,y,A,B,center,d)[2]
-    elif aixs[-1]=='ring':
-        return iiid2iid(aixs[0],x,y,A,B,center,d)[2]
-def show(aixs,center=(0,0,0),x=720,y=720,imshow=[-1],d=(1,0)):#显示(aixs：记录所有需要显示的点线面的列表,观察者观察的中心位置,显示的视图大小,imshow：A,B角度，k,l转动参数，最后一个模式设置（-1为直接展示，0为暂停，1为导出图片),d：(1,0)为正交，比例为1，(200,100)表示观察者距离中心200，距观察者100处比例为1)
-    if imshow[-1]==0:
-        A=imshow[0]
-        B=imshow[1]
-        k=imshow[2]
-        l=imshow[3]
-    elif imshow[-1]==1:
-        A=imshow[0]
-        B=imshow[1]
-        k=1
-        l=0
+    elif obj3d[-1] == "line":
+        return iiid2iid(
+            (
+                (obj3d[0][0] + obj3d[1][0]) / 2,
+                (obj3d[0][1] + obj3d[1][1]) / 2,
+                (obj3d[0][2] + obj3d[1][2]) / 2,
+            ),
+            x,
+            y,
+            A,
+            B,
+            center,
+            d,
+        )[2]
+    elif obj3d[-1] == "sphere":
+        return iiid2iid(obj3d[0], x, y, A, B, center, d)[2]
+    elif obj3d[-1] == "ring":
+        return iiid2iid(obj3d[0], x, y, A, B, center, d)[2]
+
+
+def show(
+    obj3d_list, center=(0, 0, 0), x=720, y=720, imshow=[-1], d=(1, 0)
+):  # 显示(aixs：记录所有需要显示的点线面的列表,观察者观察的中心位置,显示的视图大小,imshow：A,B角度，k,l转动参数，最后一个模式设置（-1为直接展示，0为暂停，1为导出图片),d：(1,0)为正交，比例为1，(200,100)表示观察者距离中心200，距观察者100处比例为1)
+    if imshow[-1] == 0:
+        A = imshow[0]
+        B = imshow[1]
+        k = imshow[2]
+        l = imshow[3]
+    elif imshow[-1] == 1:
+        A = imshow[0]
+        B = imshow[1]
+        k = 1
+        l = 0
     else:
-        A=90
-        B=0
-        k=1
-        l=0
-    aixs=sorted(aixs,key = lambda u:distance(u,x,y,A,B,center,d),reverse=True)#计算渲染顺序
-    while(True):
-        img=np.zeros((y,x,3),np.uint8)
-        for aix in aixs:
-            if aix[-1]=='sphere':
-                sphere(img,aix[0],x/2,y/2,A,B,aix[1],aix[2],center,aix[3],d)
-            elif aix[-1]=='line':
-                line(img,aix[0],aix[1],x/2,y/2,A,B,aix[2],aix[3],center,aix[4],d)
-            elif aix[-1]=='ring':
-                if len(aix)==5:
-                    ring(img,aix[0],x/2,y/2,A,B,aix[1],aix[2],center,aix[3],d)
-                elif len(aix)==6:
-                    ring(img,aix[0],x/2,y/2,A,B,aix[1],aix[2],center,aix[3],d,aix[4])
-            elif aix[-1]=='text':
-                cv2.putText(img,aix[0],aix[1],cv2.FONT_HERSHEY_SIMPLEX,aix[2],aix[3],aix[4])
-        A=(A+180)%360-180
-        cv2.putText(img,str(A),(0,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),1)
-        cv2.putText(img,str(B),(0,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),1)
-        if not imshow[-1]==0:
-            cv2.imshow('img',img)
-        key=cv2.waitKey(1)&0xFF
-        if key==27:#esc键退出
+        A = 90
+        B = 0
+        k = 1
+        l = 0
+    obj3d_list = sorted(
+        obj3d_list,
+        key=lambda obj3d: distance(obj3d, x, y, A, B, center, d),
+        reverse=True,
+    )  # 计算渲染顺序
+    while True:
+        img = np.zeros((y, x, 3), np.uint8)
+        for obj3d in obj3d_list:
+            if obj3d[-1] == "sphere":
+                sphere(
+                    img,
+                    obj3d[0],
+                    x / 2,
+                    y / 2,
+                    A,
+                    B,
+                    obj3d[1],
+                    obj3d[2],
+                    center,
+                    obj3d[3],
+                    d,
+                )
+            elif obj3d[-1] == "line":
+                line(
+                    img,
+                    obj3d[0],
+                    obj3d[1],
+                    x / 2,
+                    y / 2,
+                    A,
+                    B,
+                    obj3d[2],
+                    obj3d[3],
+                    center,
+                    obj3d[4],
+                    d,
+                )
+            elif obj3d[-1] == "ring":
+                if len(obj3d) == 5:
+                    ring(
+                        img,
+                        obj3d[0],
+                        x / 2,
+                        y / 2,
+                        A,
+                        B,
+                        obj3d[1],
+                        obj3d[2],
+                        center,
+                        obj3d[3],
+                        d,
+                    )
+                elif len(obj3d) == 6:
+                    ring(
+                        img,
+                        obj3d[0],
+                        x / 2,
+                        y / 2,
+                        A,
+                        B,
+                        obj3d[1],
+                        obj3d[2],
+                        center,
+                        obj3d[3],
+                        d,
+                        obj3d[4],
+                    )
+            elif obj3d[-1] == "text":
+                cv2.putText(
+                    img,
+                    obj3d[0],
+                    obj3d[1],
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    obj3d[2],
+                    obj3d[3],
+                    obj3d[4],
+                )
+        A = (A + 180) % 360 - 180
+        cv2.putText(
+            img, str(A), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1
+        )
+        cv2.putText(
+            img, str(B), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1
+        )
+        if not imshow[-1] == 0:
+            cv2.imshow("img", img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:  # esc键退出
             break
-        elif key==ord('d'):#w,a,s,d,左右上下旋转
-            A+=k
-            A=(A+180)%360-180
-            k+=1
-            l=0
-        elif key==ord('a'):
-            A-=k
-            A=(A+180)%360-180
-            k+=1
-            l=0
-        elif key==ord('w'):
-            B-=k
-            if B<-90:
-                B=-90
-            k+=1
-            l=0
-        elif key==ord('s'):
-            B+=k
-            if B>90:
-                B=90
-            k+=1
-            l=0
+        elif key == ord("d"):  # w,a,s,d,左右上下旋转
+            A += k
+            A = (A + 180) % 360 - 180
+            k += 1
+            l = 0
+        elif key == ord("a"):
+            A -= k
+            A = (A + 180) % 360 - 180
+            k += 1
+            l = 0
+        elif key == ord("w"):
+            B -= k
+            if B < -90:
+                B = -90
+            k += 1
+            l = 0
+        elif key == ord("s"):
+            B += k
+            if B > 90:
+                B = 90
+            k += 1
+            l = 0
         else:
-            l+=1
-            #print(l)
-            if l>49:
-                k=1
-                l=0
-        if imshow[-1]==0:
+            l += 1
+            # print(l)
+            if l > 49:
+                k = 1
+                l = 0
+        if imshow[-1] == 0:
             break
-    if imshow[-1]==0:
+    if imshow[-1] == 0:
         return img
-    elif imshow[-1]==1:
-        return A,B
+    elif imshow[-1] == 1:
+        return A, B
     else:
         cv2.destroyAllWindows()
--- src/pyfii/cv3d/transfer.py
+++ src/pyfii/cv3d/transfer.py
@@ -1,56 +1,152 @@
+from typing import List, Tuple
 import numpy as np
-def xy2ra(x,y):#二维坐标转化为距离方位
-    r=(x**2+y**2)**0.5
-    if y<0:
-        a=-np.arccos(x/r)
-    elif y>0:
-        a=np.arccos(x/r)
+
+Vec2 = np.ndarray | Tuple[float, float] | List[float, float]
+
+Degree = float  # 角度制浮点数
+Radian = float  # 弧度制浮点数
+
+
+def xy2ra(x: float, y: float) -> Tuple[float, Radian]:
+    """二维坐标转化为距离方位
+
+    Args:
+        x (float): x坐标
+        y (float): y坐标
+
+    Returns:
+        Tuple[float, Radian]: 距离, 角度
+    """
+    r = (x**2 + y**2) ** 0.5
+    if y < 0:
+        a = -np.arccos(x / r)
+    elif y > 0:
+        a = np.arccos(x / r)
     else:
-        if x>=0:
-            a=0
+        if x >= 0:
+            a = 0
         else:
-            a=np.pi
-    return r,a
-def ra2xy(r,a):#距离方位转化为二维坐标
-    x=r*np.cos(a)
-    y=r*np.sin(a)
-    return x,y
-def xyz2rab(x,y,z):#三维坐标转化为距离方位
-    r,a=xy2ra(x,y)
-    r,b=xy2ra(r,z)
-    return r,a,b
-def rab2xyz(r,a,b):#距离方位转化为三维坐标
-    r,z=ra2xy(r,b)
-    x,y=ra2xy(r,a)
-    return x,y,z
-def rotate(aixs,a):#二维旋转(坐标点,旋转角度)
-    x=aixs[0]
-    y=aixs[1]
-    R,A=xy2ra(x,y)
-    x,y=ra2xy(R,A+a)
-    return (x,y)
-def resize(aixs,n):#二维缩放(坐标点,缩放大小)
-    x=aixs[0]*n
-    y=aixs[1]*n
-    return (x,y)
-def move(aixs,x,y):#二维移动(坐标点,xy方向移动距离)
-    x+=aixs[0]
-    y+=aixs[1]
-    return (x,y)
-def rotate3d(aixs,a,b):#三维旋转(坐标点,旋转角度)
-    x=aixs[0]
-    y=aixs[1]
-    z=aixs[2]
-    x,y=rotate((x,y),a)
-    x,z=rotate((x,z),b)
-    return (x,y,z)
-def resize3d(aixs,n):#三维缩放(坐标点,缩放大小)
-    x=aixs[0]*n
-    y=aixs[1]*n
-    z=aixs[2]*n
-    return (x,y,z)
-def move3d(aixs,x,y,z):#三维移动(坐标点,xyz方向移动距离)
-    x+=aixs[0]
-    y+=aixs[1]
-    z+=aixs[2]
-    return (x,y,z)
+            a = np.pi
+    return r, a
+
+
+def ra2xy(r: float, a: Radian) -> Tuple[float, float]:
+    """距离方位转化为二维坐标
+
+    Args:
+        r (float): 距离
+        a (Radian): 角度
+
+    Returns:
+        Tuple[float, float]: xy坐标
+    """
+    x = r * np.cos(a)
+    y = r * np.sin(a)
+    return x, y
+
+
+def xyz2rab(
+    x: float, y: float, z: float
+) -> Tuple[float, Radian, Radian]:  # 三维坐标转化为距离方位
+    """三维坐标转化为距离方位
+
+    Args:
+        x (float): x坐标
+        y (float): y坐标
+        z (float): z坐标
+
+    Returns:
+        Tuple[float, Radian, Radian]: 距离, 方向角, 仰角
+    """
+    r, a = xy2ra(x, y)
+    r, b = xy2ra(r, z)
+    return r, a, b
+
+
+def rab2xyz(r: float, a: Radian, b: Radian) -> Tuple[float, float, float]:
+    """距离方位转化为三维坐标
+
+    Args:
+        r (float): 距离
+        a (Radian): 方向角
+        b (Radian): 仰角
+
+    Returns:
+        Tuple[float, float, float]: xyz坐标
+    """
+    r, z = ra2xy(r, b)
+    x, y = ra2xy(r, a)
+    return x, y, z
+
+
+def rotate(
+    point2d: Vec2, angle: Radian
+) -> Tuple[float, float]:  # 二维旋转(坐标点,旋转角度)
+    """二维旋转(坐标点,旋转角度)
+
+    Args:
+        point2d (Vec2): 坐标点
+        angle (Radian): 旋转角度
+
+    Returns:
+        Tuple[float, float]: xy坐标
+    """
+    x = point2d[0]
+    y = point2d[1]
+    r, a = xy2ra(x, y)
+    x, y = ra2xy(r, a + angle)
+    return (x, y)
+
+
+def resize(point2d: Vec2, scale: float) -> Tuple[float, float]:
+    """二维缩放(坐标点,缩放大小)
+
+    Args:
+        point2d (Vec2): 坐标点
+        scale (float): 缩放大小
+
+    Returns:
+        Tuple[float, float]: xy坐标
+    """
+    x = point2d[0] * scale
+    y = point2d[1] * scale
+    return (x, y)
+
+
+def move(point2d: Vec2, x: float, y: float) -> Tuple[float, float]:
+    """二维移动(坐标点,xy方向移动距离)
+
+    Args:
+        point2d (Vec2): 坐标点
+        x (float): x方向移动距离
+        y (float): y方向移动距离
+
+    Returns:
+        Tuple[float, float]: xy坐标
+    """
+    x += point2d[0]
+    y += point2d[1]
+    return (x, y)
+
+
+def rotate3d(aixs, a, b):  # 三维旋转(坐标点,旋转角度)
+    x = aixs[0]
+    y = aixs[1]
+    z = aixs[2]
+    x, y = rotate((x, y), a)
+    x, z = rotate((x, z), b)
+    return (x, y, z)
+
+
+def resize3d(aixs, n):  # 三维缩放(坐标点,缩放大小)
+    x = aixs[0] * n
+    y = aixs[1] * n
+    z = aixs[2] * n
+    return (x, y, z)
+
+
+def move3d(aixs, x, y, z):  # 三维移动(坐标点,xyz方向移动距离)
+    x += aixs[0]
+    y += aixs[1]
+    z += aixs[2]
+    return (x, y, z)
