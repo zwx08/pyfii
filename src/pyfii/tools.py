eb135213182376e87db4a282844a2d55c4323917
zwx08 <zwx080104@gmail.com>
2024-06-15 17:24:00 +0800

    添加Drone_first方法

---
+++ src/pyfii/tools.py
@@ -0,0 +1,8 @@
+import drone
+def Drone_first(X: int,Y: int,Z: int,vel: int=200,acc:int=400,drone_config=drone.drone_config_6m):
+    d=drone.Drone(X,Y,drone_config)
+    d.takeoff(1,Z)
+    d.inittime(4)
+    d.VelXY(vel,acc)
+    d.VelZ(vel,acc)
+    return d
