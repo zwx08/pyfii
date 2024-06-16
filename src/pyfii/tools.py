90fb3d441a8344550a714b9b0e91b9972feb9836
zwx08 <zwx080104@gmail.com>
2024-06-16 11:03:02 +0800

    tool test02

--- src/pyfii/tools.py
+++ src/pyfii/tools.py
@@ -1,4 +1,4 @@
-import drone
+from . import drone
 def Drone_first(X: int,Y: int,Z: int,vel: int=200,acc:int=400,drone_config=drone.drone_config_6m):
     d=drone.Drone(X,Y,drone_config)
     d.takeoff(1,Z)
