b35a3e351afc194a70240c93b5896a86dba4d85d
zwx08 <zwx080104@gmail.com>
2024-06-16 11:06:31 +0800

    tool(Drone_first) test02

--- src/pyfii/tools.py
+++ src/pyfii/tools.py
@@ -1,5 +1,8 @@
 from . import drone
-def Drone_first(X: int,Y: int,Z: int,vel: int=200,acc:int=400,drone_config=drone.drone_config_6m):
+def Drone_first(first_coords: tuple,vel: int=200,acc:int=400,drone_config=drone.drone_config_6m):
+    X=first_coords[0]
+    Y=first_coords[1]
+    Z=first_coords[2]
     d=drone.Drone(X,Y,drone_config)
     d.takeoff(1,Z)
     d.inittime(4)
